41b6e22fd8505881d23faadd6078fe0965245ef2
m-posieczek <108526727+m-posieczek@users.noreply.github.com>
2022-09-27 08:13:17 +0200

    heroes app angular tutorial

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,13 @@
+import { Hero } from "./hero";
+
+export const HEROES: Hero[] = [
+    { id: 12, name: 'Dr. Nice' },
+    { id: 13, name: 'Bombasto' },
+    { id: 14, name: 'Celeritas' },
+    { id: 15, name: 'Seba' },
+    { id: 16, name: 'BurgerKing Foot' },
+    { id: 17, name: 'Dynama' },
+    { id: 18, name: 'Dr. 32 IQ' },
+    { id: 19, name: 'Kacper' },
+    { id: 20, name: 'Wiktor' }
+];
